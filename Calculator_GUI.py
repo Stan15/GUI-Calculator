c6f8ba4ae6c4c292d6744a68335d85e29b588a53
Stan15 <47716543+Stan15@users.noreply.github.com>
2019-12-10 22:55:54 -0500

    "syntax ERROR", "not supported yet" messages added.
    summary: "syntax ERROR" message added and disabled some features which are not fully fleshed out
    
    Changes:
    -added "syntax ERROR" message when:
            -multiple operators are next to each other "×÷" or "÷÷". (to be fixed not to apply to addition and subtraction)
            -multiple decimal points are next to each other ".."
            -an operator appears at the start of a statement "÷46+7" or "4×9×" or "×".
    -disabled the alternate "SHIFT" values of "1" and "2" and replaced them with a "not supported net" message
    -also added "not supported net" error message of the "CE", "ANS", and "x^2" buttons
    -set cursor to move to end of display when the equal sign is clicked
    
    Fixes:
    -fixed display output bug which occurs after pressing "DEL" button when the display is already cleared.

--- Calculator_GUI.py
+++ Calculator_GUI.py
@@ -1,12 +1,28 @@
-from tkinter import Button, Label, Tk, Text, INSERT, END
+from tkinter import Button, Label, Tk, Text, INSERT, END, messagebox
 
 button_height = 3
 button_width = 5
 
 output = "|"
+operators = ["-", "+", "÷", "×"]
 shiftval = 0               #is "SHIFT" pressed or not?
 alphaval = 0               #is "ALPHA" pressed or not?
 position = len(output)-1   #position of cursor
+
+def set_disp_to_output():
+    global output
+    display.config(state="normal")
+    display.delete('1.0', END)
+    display.insert(INSERT, output)
+    display.config(state="disabled")
+
+def error_msg(error_type):
+    error_color=display.tag_config('error', foreground="red")
+    display.config(state="normal")
+    display.insert(END, "\n\n" + error_type + " ERROR", "error")
+    display.config(state="disabled")
+    display.update_idletasks
+    display.after(1000, set_disp_to_output)
 
 def shift_comm():
     global shiftval
@@ -32,10 +48,7 @@ def left_comm():
         output = output.replace("|", "")
         position = position-1
         output = output[:position] + "|" + output[position:]
-        display.config(state="normal")
-        display.delete('1.0', END)
-        display.insert(INSERT, output)
-        display.config(state="disabled")
+        set_disp_to_output()
 
 def right_comm():
     global output, position
@@ -44,44 +57,73 @@ def right_comm():
         output = output.replace("|", "")
         position = position+1
         output = output[:position] + "|" + output[position:]
-        display.config(state="normal")
-        display.delete('1.0', END)
-        display.insert(INSERT, output)
-        display.config(state="disabled")
+        set_disp_to_output()
 
 def num_disp(norm_imp="", shift_imp=None):
     global output, position
-    if (shiftval==1) and (not (shift_imp is None)):        #if shift is pressed and the key has a "SHIFT" value
-        text=shift_imp
+    if (shiftval==1) and (shift_imp is not None):        #if shift is pressed and the key has a "SHIFT" value
+        #text=shift_imp         ###### TO-DO
+        unsupported()  #to be removed
+        shift_comm()   #to be removed
+        return         #to be removed                                              
     else:
         text=norm_imp
 
     output = output[:position] + text + output[position:]
     position=position+len(text)
-    display.config(state="normal")
-    display.delete('1.0', END)
-    display.insert(INSERT, output)
-    display.config(state="disabled")
+    set_disp_to_output()
 
 def clear_disp():
     global output, position
 
     output="|"
     position=0
-    display.config(state="normal")
-    display.delete('1.0', END)
-    display.insert(INSERT, output)
-    display.config(state="disabled")
+    set_disp_to_output()
 
 def delete_key():
     global output, position
 
-    output = output[:position-1] + output[position:]
-    position=position-1
-    display.config(state="normal")
-    display.delete('1.0', END)
-    display.insert(INSERT, output)
-    display.config(state="disabled")
+    if position != 0:
+        output = output[:position-1] + output[position:]
+        position=position-1
+        set_disp_to_output()
+
+def equals_sign():
+    global output, operators, position
+    precedence=[]
+    op_index=[]
+
+    if position != len(output):
+        output = output.replace("|", "")
+        position = len(output)
+        output = output[:position] + "|" + output[position:]
+        set_disp_to_output()
+
+
+
+    if len(output) == 0:
+        return
+    #------------------error check--------------------
+    
+    errorcheck=None                                         #placeholder. for storing the index of the previous operator(for error checking purpose))
+    for i in range(len(output)):
+        if (output[i] in operators):
+            if (errorcheck == i-1) or (i==len(output)-2) or (i==0):    #if two operators are next to each other    
+                error_msg("syntax")                          #or if an operator is on the end of output, print syntax error message 
+                return
+            errorcheck = i
+
+        if (output[i]) == ".":
+            try:                            
+                if output[i+1] == ".":                       #if two dots next to each other, display syntax error
+                    error_msg("syntax")                
+                    return
+            except IndexError:                               #except if dot is at the end of line, in which case there is only one dot present
+                pass
+
+
+def unsupported():      #to be removed
+    messagebox.showinfo("error", "not supported yet")  #to be removed
 #---------------------------------Main Window Script-----------------------------------------------
 
 root=Tk()
@@ -113,14 +155,14 @@ multiply = Button(root, text="×", command=lambda: num_disp("×"), height=button
 divide = Button(root, text="÷", command=lambda: num_disp("÷"), height=button_height, width=button_width, bg="#d6d6d6")
 plus = Button(root, text="+", command=lambda: num_disp("+"), height=button_height, width=button_width, bg="#d6d6d6")
 minus = Button(root, text="-", command=lambda: num_disp("-"), height=button_height, width=button_width, bg="#d6d6d6")
-equals = Button(root, text="=", height=button_height, width=button_width, bg="#d6d6d6")
-answer = Button(root, text="ANS", height=button_height, width=button_width, bg="#d6d6d6")
-square = Button(root, text="√     \nx^2", command=lambda: num_disp("^(2)", "√"), height=button_height, width=button_width)
+equals = Button(root, text="=", command=equals_sign, height=button_height, width=button_width, bg="#d6d6d6")
+answer = Button(root, text="ANS", command=unsupported, height=button_height, width=button_width, bg="#d6d6d6")
+square = Button(root, text="√     \nx^2", command=unsupported, height=button_height, width=button_width)
 
 #Functions
 alpha = Button(root, text="ALPHA", command=alpha_comm, height=button_height-1, width=button_width, bg="#593d3d", fg="#ffffff")
 shift = Button(root, text="SHIFT", command=shift_comm, height=button_height-1, width=button_width, bg="#4a3f2b", fg="#ffffff")
-clear_all = Button(root, text="CE", height=button_height-1, width=button_width, bg="#4a4a4a", fg="#ffffff")
+clear_all = Button(root, text="CE", command=unsupported, height=button_height-1, width=button_width, bg="#4a4a4a", fg="#ffffff")
 right = Button(root, text="▷", command=right_comm, height=button_height-1, width=button_width, bg="#828282", fg="#ffffff")
 left = Button(root, text="◁", command=left_comm, height=button_height-1, width=button_width, bg="#828282", fg="#ffffff")
 #-------------------------------------GUI Def------------------------------------
